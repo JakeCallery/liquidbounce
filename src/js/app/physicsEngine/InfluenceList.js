6de737a9412bc2b7ebe023a65157161993e1e740
Jake Callery <jcallery@subvoicestudios.com>
2013-05-29 10:11:34 -0400

    minor refactor/code clean up

--- src/js/app/physicsEngine/InfluenceList.js
+++ src/js/app/physicsEngine/InfluenceList.js
@@ -4,37 +4,48 @@
  */
 
 define([
-'jac/utils/ArrayUtils'],
-function(ArrayUtils){
+'jac/utils/ArrayUtils',
+'jac/math/Vec2D'],
+function(ArrayUtils, Vec2D){
     return (function(){
         /**
          * Creates a InfluenceList object
          * @constructor
          */
         function InfluenceList(){
-            this.list = [];
+	        /**
+	         * @private
+	         * @type {Array}
+	         */
+	        this._list = [];
+
+	        /**
+	         * @public
+	         * @type {Vec2DObj}
+	         */
+	        this.cachedResult = new Vec2DObj(0,0);
         }
 
 	    InfluenceList.prototype.clear = function(){
-		    this.list.length = 0;
+		    this._list.length = 0;
 	    };
 
 	    InfluenceList.prototype.removeAllBut = function($influenceObj){
-		    this.list.length = 0;
-		    this.list.push($influenceObj);
+		    this._list.length = 0;
+		    this._list.push($influenceObj);
 	    };
 
 	    InfluenceList.prototype.addInfluence = function($influenceObj){
 	        //TODO: Add duplicate checking
-		    this.list.push($influenceObj);
+		    this._list.push($influenceObj);
 	    };
 
 	    InfluenceList.prototype.getInfluenceById = function($id){
-			return ArrayUtils.findFirstObjWithProp(this.list, 'id', $id);
+			return ArrayUtils.findFirstObjWithProp(this._list, 'id', $id);
 	    };
 
 	    InfluenceList.prototype.getAllInfluencesById = function($id){
-			return ArrayUtils.findAllObjsWithProp(this.list, 'id', $id);
+			return ArrayUtils.findAllObjsWithProp(this._list, 'id', $id);
 	    };
 
         //Return constructor
