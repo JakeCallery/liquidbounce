0a92c43a2fbc6d1dc091f626e82837c6d93f53c1
Jake Callery <jcallery@subvoicestudios.com>
2013-06-11 18:03:57 -0400

    bounce vector calc finished

--- src/js/app/game/managers/CollisionManager.js
+++ src/js/app/game/managers/CollisionManager.js
@@ -107,18 +107,18 @@ function(EventDispatcher,ObjUtils,IManager,L,Rectangle,Vec2D,Vec2DObj,DebugDrawT
 						var shellVec = null;
 						var baseVec = new Vec2DObj(0,0,0,0);
 						var prevBaseVec = new Vec2DObj(0,0,0,0);
-						var shellVecLn = null;
+						var shellVecLeftNormal = null;
 
 					    //check against all shell vects
 					    for(var v = 0, c = obj.shellVecList.length; v < c; v++){
 						    shellVec = obj.shellVecList[v];
-						    shellVecLn = Vec2D.duplicate(shellVec);
-						    Vec2D.calcLeftNormal(shellVecLn, shellVec);
+						    shellVecLeftNormal = Vec2D.duplicate(shellVec);
+						    Vec2D.calcLeftNormal(shellVecLeftNormal, shellVec);
 						    Vec2D.vecFromLineSeg(prevBaseVec, bp.prevX, bp.prevY, shellVec.xOffset, shellVec.yOffset);
 						    Vec2D.vecFromLineSeg(baseVec, bp.x, bp.y, shellVec.xOffset, shellVec.yOffset);
 						    var dp1 = Vec2D.scaledDot(baseVec, shellVec);
-						    var dp2 = Vec2D.scaledDot(baseVec, shellVecLn);
-						    var dp3 = Vec2D.scaledDot(prevBaseVec, shellVecLn);
+						    var dp2 = Vec2D.scaledDot(baseVec, shellVecLeftNormal);
+						    var dp3 = Vec2D.scaledDot(prevBaseVec, shellVecLeftNormal);
 
 						    L.log('DP1: ' + dp1, '@collision');
 						    L.log('DP2: ' + dp2, '@collision');
@@ -129,7 +129,7 @@ function(EventDispatcher,ObjUtils,IManager,L,Rectangle,Vec2D,Vec2DObj,DebugDrawT
 						    ddt.drawLine(blobMoveVec.xOffset, blobMoveVec.yOffset, (blobMoveVec.x + blobMoveVec.xOffset), (blobMoveVec.y + blobMoveVec.yOffset), '#00FFFF');
 						    ddt.drawLine(baseVec.xOffset, baseVec.yOffset, (baseVec.x + baseVec.xOffset), (baseVec.y + baseVec.yOffset), '#FFFFFF');
 						    ddt.drawLine(shellVec.xOffset, shellVec.yOffset, (shellVec.x + shellVec.xOffset), (shellVec.y + shellVec.yOffset), '#FF00FF');
-						    ddt.drawLine(shellVecLn.xOffset, shellVecLn.yOffset, (shellVecLn.x + shellVecLn.xOffset), (shellVecLn.y + shellVecLn.yOffset), '#FFFF00');
+						    ddt.drawLine(shellVecLeftNormal.xOffset, shellVecLeftNormal.yOffset, (shellVecLeftNormal.x + shellVecLeftNormal.xOffset), (shellVecLeftNormal.y + shellVecLeftNormal.yOffset), '#FFFF00');
 						    ////////////////////
 
 						    //Check to see if the point is within the scope of the shell vector line segment
@@ -164,24 +164,24 @@ function(EventDispatcher,ObjUtils,IManager,L,Rectangle,Vec2D,Vec2DObj,DebugDrawT
 								    var moveLen = Vec2D.lengthOf(blobMoveVec);
 								    var colForceX = (blobMoveVec.x / moveLen) * Math.abs(dp2);
 								    var colForceY = (blobMoveVec.y / moveLen) * Math.abs(dp2);
-								    var colVec = new Vec2DObj(colForceX, colForceY);
+								    //var colVec = new Vec2DObj(colForceX, colForceY);
 									bp.x -= colForceX;
 								    bp.y -= colForceY;
 
 								    //Calc bounce vector
-									var movecolDP = Vec2D.scaledDot(blobMoveVec, colVec);
+									var movecolDP = Vec2D.scaledDot(blobMoveVec, shellVec);
 
 								    //Find projections
-								    var colLen = Vec2D.lengthOf(colVec);
-								    var colLeftNormal = Vec2D.duplicate(colVec);
-								    Vec2D.calcLeftNormal(colLeftNormal, colVec);
-								    var p1_x = movecolDP * colVec.x / colLen;
-								    var p1_y = movecolDP * colVec.y / colLen;
+								    var shellVecLength = Vec2D.lengthOf(shellVec);
+								    //var colLeftNormal = Vec2D.duplicate(shellVec);
+								    //Vec2D.calcLeftNormal(colLeftNormal, shellVec);
+								    var p1_x = movecolDP * shellVec.x / shellVecLength;
+								    var p1_y = movecolDP * shellVec.y / shellVecLength;
 
-								    var movcollenDP = Vec2D.scaledDot(blobMoveVec, colLeftNormal);
-									var colLeftNormalLen = Vec2D.lengthOf(colLeftNormal);
-								    var p2_x = movcollenDP * colLeftNormal.x / colLeftNormalLen;
-								    var p2_y = movcollenDP * colLeftNormal.y / colLeftNormalLen;
+								    var movColLeftNormalDP = Vec2D.scaledDot(blobMoveVec, shellVecLeftNormal);
+									var colLeftNormalLen = Vec2D.lengthOf(shellVecLeftNormal);
+								    var p2_x = movColLeftNormalDP * shellVecLeftNormal.x / colLeftNormalLen;
+								    var p2_y = movColLeftNormalDP * shellVecLeftNormal.y / colLeftNormalLen;
 
 								    //Reverse projections
 								    p2_x *= -1;
@@ -192,7 +192,7 @@ function(EventDispatcher,ObjUtils,IManager,L,Rectangle,Vec2D,Vec2DObj,DebugDrawT
 								    var bounce_y = p1_y + p2_y;
 
 								    //Reset prevX/Y to 'bounce' the particle
-									ddt.drawLine(bp.x, bp.y, bp.x + bounce_x * 20, bp.y + bounce_y * 20,'#FFFF00');
+									ddt.drawLine(bp.x, bp.y, bp.x + bounce_x * 5, bp.y + bounce_y * 5,'#FFFF00');
 
 							    }
 
